56d4dd8cae0d3af99e8ebc26e92f4db6a0806c70
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-30 12:07:45 -0700

    added more routes

--- server/router/router.js
+++ server/router/router.js
@@ -1,8 +1,26 @@
 var router = require('express').Router();
 var controllers = require('../controllers/controllers.js');
 
+router.post('/api/favorites', controllers.favorites.post);
+
+router.get('/api/favorites', controllers.favorites.get);
+
+router.delete('/api/favorites', controllers.favorites.delete);
+
 router.get('/api/colleges', controllers.colleges.get);
 
-router.post('/api/colleges/suggestions', controllers.colleges.getSuggestions);
+router.post('/api/colleges/', controllers.colleges.getSuggestions);
+
+router.post('/auth', controllers.auth.post);
+
+router.post('/signup', controllers.signup.post);
+
+router.post('/login', controllers.login.post);
+
+router.post('/logout', controllers.logout.post);
+
+
+
+
 
 module.exports = router;
