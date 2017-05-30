475820018d71ac777258574409f98bc2df1a8edc
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-30 12:10:06 -0700

    added sessions file and updated models

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ var router = require('./router/router.js');
 var sessions = require('./sessions.js');
 var models = require('./models/models.js');
 
-
 var app = express();
 var IP = process.env.IP || 'localhost';
 var PORT = process.env.PORT || 3000;
@@ -24,12 +23,9 @@ app.get('*', function(request, response) {
   response.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-
-
 app.listen(PORT, function () {
   console.log('listening right now on port', PORT);
 });
-
 
 console.log('listening on', IP, PORT);
 
--- server/models/models.js
+++ server/models/models.js
@@ -2,20 +2,21 @@ var connection = require('./dbConnection').connection;
 var mySearchFunction = require('./queryHelper.js').mySearchFunction;
 var asyncMap = require('./queryHelper.js').asyncMap;
 var querySchoolTable = require('./queryHelper.js').querySchoolTable;
+var bcrypt = require('bcrypt-nodejs');
 
 module.exports = {
   colleges: {
-    get: function(cb) {
-      connection.query('SELECT name, address, state, description, admission_rate, tuition, size, average_gpa, average_sat_score, sports_division, website_url, image_url FROM Universities', function(err, results, fields) {
+    get: function (cb) {
+      connection.query('SELECT * FROM Universities', function (err, results, fields) {
         if (err) {
-          cb (err, null);
+          cb(err, null);
         } else {
           cb(null, results);
         }
       });
     },
-    getSuggestions: function(params, cb) {
-      mySearchFunction(params, function(err, data) {
+    getSuggestions: function (params, cb) {
+      mySearchFunction(params, function (err, data) {
         if (err) {
           cb(err, null);
         } else {
@@ -23,5 +24,111 @@ module.exports = {
         }
       });
     }
-  }
+  },
+  signup: {
+    post: function (username, password, cb) {
+      console.log('beginning of model function for signup');
+      connection.query('SELECT * from Users Where username = ?', username, function (err, results, fields) {
+        if (err) {
+          cb(err, null);
+        } else {
+          if (results.length !== 0) {
+            console.log('USER EXISTS', results);
+            cb('User already exists', null);
+          } else {
+            // hash password and store
+            let salt = bcrypt.genSaltSync(10);
+            bcrypt.hash(password, salt, null, function (err, hash) {
+              if (err) {
+                console.log('Error hashing password', err);
+              } else {
+                connection.query('Insert into Users (username, password) Values (?, ?)', [username, hash], function (err, results, fields) {
+                  if (err) {
+                    cb(err, null);
+                  } else {
+                    cb(null, 'User successfully created');
+                  }
+                });
+              }
+            });
+          }
+        }
+      });
+    }
+  },
+
+  login: {
+    post: function (username, password, cb) {
+      // update the logic. Get hashed password (err if null) => bcrypt compare if they match 
+      connection.query('Select password from Users where username = ?', [username], function (err, results, fields) {
+        if (err) {
+          cb(err, null);
+        } else {
+          if (results.length === 0) {
+            cb('Wrong login or password', null);
+          } else {
+            let retrievedPassword = results[0].password;
+            bcrypt.compare(password, retrievedPassword, function (err, result) {
+              if (err) {
+                cb('Wrong login or password', null);
+              } else {
+                if (result === true) {
+                  cb(null, 'User successfully logged in Models');
+                } else {
+                  cb('Wrong login or password', null);
+                }
+              }
+            });
+          }
+        }
+      });
+    }
+  },
+
+  favorites: {
+    post: function (username, collegeID, callback) {
+      connection.query('select id from users where username = ?', username, function (error, rows, fields) {
+        var id = JSON.parse(JSON.stringify(rows))[0].id;
+        console.log('userID is ', id);
+        var dbArray = [id, collegeID];
+        connection.query('insert into favoriteus set user_id = ?, university_id = ?', dbArray, function (error, rows, fields) {
+          if (!error) {
+            console.log('favorite added');
+            callback(null, 'Favorite added by the model');
+          }
+        });
+      });
+    },
+    get: function (username, cb) {
+      connection.query('SELECT * FROM universities JOIN  favoriteus ON universities.id = favoriteus.university_id JOIN users ON users.id = favoriteus.user_id WHERE users.username = ?', username, function (err, results, fields) {
+        if (err) {
+          cb(err, null);
+        } else {
+          cb(null, results);
+        }
+      });
+    },
+    delete: function (username, collegeID, callback) {
+      connection.query('select id from users where username = ?', username, function (error, rows, fields) {
+        var id = JSON.parse(JSON.stringify(rows))[0].id;
+        console.log('userID to delete a favorite is ', id);
+        var dbArray = [id, collegeID];
+        console.log('DBARRAY --->', dbArray);
+        connection.query('DELETE FROM favoriteus WHERE user_id = ? AND university_id = ?', dbArray, function (error, rows, fields) {
+          if (error) {
+            console.log('error in deleting user says the model');
+          } else {
+            console.log('rows after deleting from DB ', rows);
+            connection.query('SELECT * FROM universities JOIN  favoriteus ON universities.id = favoriteus.university_id JOIN users ON users.id = favoriteus.user_id WHERE users.username = ?', username, function (err, results, fields) {
+              if (!err) {
+                callback(null, JSON.stringify(results));
+              } else {
+                callback(err, null);
+              }
+            });
+          }
+        });
+      });
+    }
+  },
 };
